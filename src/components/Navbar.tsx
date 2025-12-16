16cccf84dce7e3b2e99f3c329892b40cbb2a4cf3
elvis Karl <elviskarl343@gmail.com>
2025-12-16 14:40:00 +0300

    styled the navbar by structuring the html styling it.

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,19 +1,24 @@
 import { NavLink, Outlet } from "react-router-dom";
+import "./styles/index.css";
 
 function Navbar() {
   return (
     <>
       <nav>
-        <section>
-          <div className="left-nav-section"></div>
-          <div className="main-nav-section">
-            <ul>
-              <NavLink to="/">Home</NavLink>
-              <NavLink to="/map">Map</NavLink>
-              <NavLink to="/about">About</NavLink>
-            </ul>
-          </div>
-        </section>
+        <div className="left-nav-section">
+          <a href="#">Hello</a>
+        </div>
+        <ul className="main-nav-section">
+          <li>
+            <NavLink to="/">Home</NavLink>
+          </li>
+          <li>
+            <NavLink to="/map">Map</NavLink>
+          </li>
+          <li>
+            <NavLink to="/about">About</NavLink>
+          </li>
+        </ul>
       </nav>
       <Outlet />
     </>
