f0f2b19dbc2a38f72fcc2b3fc1017b5dd4034670
elvis Karl <elviskarl343@gmail.com>
2026-01-08 14:35:02 +0300

    extracted exif metadata from the image and previewed it with it's attributes

--- src/pages/map/components/sidebar/components/ReportForm.tsx
+++ src/pages/map/components/sidebar/components/ReportForm.tsx
@@ -1,10 +1,29 @@
-import { useState, useRef, type ChangeEvent } from "react";
+export interface CoordinateData {
+  GPSLatitude: number;
+  GPSLongitude: number;
+  GPSLatitudeRef: "N" | "S";
+  GPSLongitudeRef: "E" | "W";
+  DateTimeOriginal: string;
+}
+
+import { useState, useRef, useEffect, type ChangeEvent } from "react";
 import ReportPreview from "./ReportPreview";
+import * as ExifReader from "exifreader";
 
 export default function ReportForm() {
   const [file, setFile] = useState<File | null>(null);
-  const [imageUrl, setImageUrl] = useState("");
+  const [imageUrl, setImageUrl] = useState<string | null>(null);
+  const [coordinates, setCoordinates] = useState<CoordinateData | null>(null);
   const fileInputRef = useRef<HTMLInputElement>(null);
+
+  useEffect(() => {
+    if (!imageUrl) return;
+
+    return () => {
+      URL.revokeObjectURL(imageUrl);
+    };
+  }, [imageUrl]);
+
   function handleSubmit(formData: FormData) {
     const imageData = formData.get("file");
     console.log(imageData);
@@ -15,6 +34,32 @@ export default function ReportForm() {
     if (files.length > 0) {
       const file = files[0];
       const url = URL.createObjectURL(file);
+      ExifReader.load(file).then((data) => {
+        if (
+          !data.DateTimeOriginal ||
+          !data.GPSLatitude ||
+          !data.GPSLatitudeRef ||
+          !data.GPSLongitude ||
+          !data.GPSLongitudeRef
+        )
+          return;
+        const DateTimeOriginal = data.DateTimeOriginal.description;
+        let GPSLatitude = data.GPSLatitude.description;
+        let GPSLongitude = data.GPSLongitude.description;
+        const GPSLatitudeRef = data.GPSLatitudeRef.value[0] as "N" | "S";
+        const GPSLongitudeRef = data.GPSLongitudeRef.value[0] as "E" | "W";
+
+        if (GPSLatitudeRef === "S") GPSLatitude *= -1;
+        if (GPSLongitudeRef === "W") GPSLongitude *= -1;
+
+        setCoordinates({
+          DateTimeOriginal,
+          GPSLatitude: Number(GPSLatitude),
+          GPSLatitudeRef,
+          GPSLongitude: Number(GPSLongitude),
+          GPSLongitudeRef,
+        });
+      });
       setImageUrl(url);
       setFile(file);
     }
@@ -36,15 +81,21 @@ export default function ReportForm() {
             />
             <div className="drop-area-container">
               <span> Drag & Drop or </span>
-              <button onClick={handleClick} className="browse-btn">
+              <button
+                onClick={handleClick}
+                className="browse-btn"
+                type="button"
+              >
                 Browse for a file
               </button>
             </div>
           </div>
+          {file && imageUrl && coordinates && (
+            <ReportPreview url={imageUrl} coordinateData={coordinates} />
+          )}
           <button className="submit-button">submit</button>
         </form>
       </div>
-      {file && <ReportPreview url={imageUrl} />}
     </>
   );
 }
--- src/pages/map/components/sidebar/components/ReportPreview.tsx
+++ src/pages/map/components/sidebar/components/ReportPreview.tsx
@@ -1,10 +1,54 @@
+import type { CoordinateData } from "./ReportForm";
+
 interface Params {
   url: string;
+  coordinateData: CoordinateData;
 }
 export default function ReportPreview(props: Params) {
+  const {
+    DateTimeOriginal,
+    GPSLatitude,
+    GPSLatitudeRef,
+    GPSLongitude,
+    GPSLongitudeRef,
+  } = props.coordinateData;
+  const dateTaken = DateTimeOriginal.split(" ");
   return (
-    <div className="image-preview-container">
-      <img src={props.url} alt="Road Damage" className="preview-image" />
-    </div>
+    <>
+      <div className="image-preview-container">
+        <img src={props.url} alt="Road Damage" className="preview-image" />
+      </div>
+      <div className="image-details-container">
+        <table>
+          <caption>Image Metadata</caption>
+          <thead>
+            <tr>
+              <th>Property</th>
+              <th>Value</th>
+              <th>Orientation</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>GPS Latitude</td>
+              <td>{GPSLatitude.toFixed(4)}</td>
+              <td>{GPSLatitudeRef}</td>
+            </tr>
+            <tr>
+              <td>GPS Longitude</td>
+              <td>{GPSLongitude.toFixed(4)}</td>
+              <td>{GPSLongitudeRef}</td>
+            </tr>
+          </tbody>
+          <tfoot>
+            <tr>
+              <td>Date Taken</td>
+              <td>{dateTaken[0].split(":").join("-")}</td>
+              <td>N/A</td>
+            </tr>
+          </tfoot>
+        </table>
+      </div>
+    </>
   );
 }
