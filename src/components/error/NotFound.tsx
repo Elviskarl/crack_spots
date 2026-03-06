ce75d715c00844e6adb28988ddb8d184a0616049
elvis <elviskarl343@gmail.com>
2026-03-06 15:31:55 +0300

    Improved the not-found fallback UI and changed its background image

--- src/components/error/NotFound.tsx
+++ src/components/error/NotFound.tsx
@@ -1,18 +1,18 @@
 import { Link } from "react-router-dom";
 import "./styles/index.css";
+// import notFoundImage from "../../assets/not_found.jpg";
 
 function NotFound() {
   return (
     <>
       <section className="not_found_container">
-        <p>
-          <span className="error_status">404</span>
-          This route doesn't exist.
-        </p>
-        <p>
-          Back to
-          <Link to="/">Homepage </Link>
-        </p>
+        <div className="not-found-content">
+          <h2>Oops, Wrong Turn...</h2>
+          <p>We can't find the page you are looking for</p>
+          <Link to="/" className="navigate-link">
+            Back to Home
+          </Link>
+        </div>
       </section>
     </>
   );
