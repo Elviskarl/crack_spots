9e56e1c001cb82ebaab90f33ef01118d609e91c4
elvis <elviskarl343@gmail.com>
2026-03-10 21:17:42 +0300

    added a pulse animation to the map container, to indicate to the user that reports are being fetched from the database

--- src/pages/map/components/leaflet-map/LeafletMap.tsx
+++ src/pages/map/components/leaflet-map/LeafletMap.tsx
@@ -14,7 +14,7 @@ import { ReportsContainer } from "./ReportsContainer";
 import { MapContext } from "../../../../context/createMapContext";
 
 function LeafletMap() {
-  const { reports } = useContext(ReportContext)!;
+  const { reports, isLoading } = useContext(ReportContext)!;
 
   function FlyToReport() {
     const map = useMap();
@@ -22,7 +22,7 @@ function LeafletMap() {
 
     useEffect(() => {
       if (!selectedReport) {
-        console.warn("No report selected");
+        // console.warn("No report selected");
         return;
       }
 
@@ -87,32 +87,36 @@ function LeafletMap() {
   }
 
   return (
-    <MapContainer
-      center={[-1.216013888888889, 36.90145277777778]}
-      zoom={14}
-      scrollWheelZoom={false}
-      zoomControl={false}
-    >
-      <LayersControl position="topright">
-        <ZoomControl position="topright" />
-        <LayersControl.BaseLayer name="OpenStreetMap">
-          <TileLayer
-            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-          />
-        </LayersControl.BaseLayer>
-        <LayersControl.BaseLayer name="Satellite View" checked>
-          <TileLayer
-            url="https://{s}.google.com/vt/lyrs=s&x={x}&y={y}&z={z}"
-            maxZoom={20}
-            subdomains={["mt1", "mt2", "mt3"]}
-          />
-        </LayersControl.BaseLayer>
-      </LayersControl>
-      <FlyToReport />
-      <ResizeMap />
-      {reports && reports.length > 0 && <ReportsContainer reports={reports} />}
-    </MapContainer>
+    <div className={`map-container ${isLoading && "pulse-animation"}`}>
+      <MapContainer
+        center={[-1.216013888888889, 36.90145277777778]}
+        zoom={14}
+        scrollWheelZoom={false}
+        zoomControl={false}
+      >
+        <LayersControl position="topright">
+          <ZoomControl position="topright" />
+          <LayersControl.BaseLayer name="OpenStreetMap">
+            <TileLayer
+              attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+              url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            />
+          </LayersControl.BaseLayer>
+          <LayersControl.BaseLayer name="Satellite View" checked>
+            <TileLayer
+              url="https://{s}.google.com/vt/lyrs=s&x={x}&y={y}&z={z}"
+              maxZoom={20}
+              subdomains={["mt1", "mt2", "mt3"]}
+            />
+          </LayersControl.BaseLayer>
+        </LayersControl>
+        <FlyToReport />
+        <ResizeMap />
+        {reports && reports.length > 0 && (
+          <ReportsContainer reports={reports} />
+        )}
+      </MapContainer>
+    </div>
   );
 }
 
