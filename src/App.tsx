cfe9c13c27b8f4dccc244ae7ef7f0cbe0c6e88a6
elvis <elviskarl343@gmail.com>
2026-03-11 11:26:43 +0300

    update, removed browser router and implemented hashrouter to handle client side reloads

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,23 @@
-import {
-  createBrowserRouter,
-  createRoutesFromElements,
-  Route,
-  RouterProvider,
-} from "react-router-dom";
+import { HashRouter, Route, Routes } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Home from "./pages/home/Home";
 import Map from "./pages/map/Map";
 import About from "./pages/about/About";
 import NotFound from "./components/error/NotFound";
 
-const router = createBrowserRouter(
-  createRoutesFromElements(
-    <Route path="/" element={<Navbar />}>
-      <Route index element={<Home />} />
-      <Route path="map" element={<Map />} />
-      <Route path="about" element={<About />} />
-      <Route path="*" element={<NotFound />} />
-    </Route>
-  ), { basename: "/crack_spots"}
-);
 function App() {
-  return <RouterProvider router={router} />;
+  return (
+    <HashRouter>
+      <Routes>
+        <Route path="/" element={<Navbar />}>
+          <Route index element={<Home />} />
+          <Route path="map" element={<Map />} />
+          <Route path="about" element={<About />} />
+          <Route path="*" element={<NotFound />} />
+        </Route>
+      </Routes>
+    </HashRouter>
+  );
 }
 
 export default App;
