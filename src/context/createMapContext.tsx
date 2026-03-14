ff82a122943df889a51b900dd452cad6721dac52
elvis <elviskarl343@gmail.com>
2026-03-14 19:01:00 +0300

    Update, saved the shapefile, as well as isNotInNairobi state to context

--- src/context/createMapContext.tsx
+++ src/context/createMapContext.tsx
@@ -5,12 +5,18 @@ import {
   type SetStateAction,
 } from "react";
 import type { Report } from "../pages/map/types";
+import type { FeatureCollection, MultiPolygon, Polygon } from "geojson";
 
 interface MapContextType {
   selectedReport: Report | null;
   setSelectedReport: Dispatch<SetStateAction<Report | null>>;
   lastFlownReport: RefObject<string | null>;
   markerRefs: RefObject<Record<string, L.Marker>>;
+  nairobiSubCountyShapefile: RefObject<FeatureCollection<
+    Polygon | MultiPolygon
+  > | null>;
+  isNotInNairobi: boolean;
+  setIsNotInNairobi: Dispatch<SetStateAction<boolean>>;
 }
 
 export const MapContext = createContext<MapContextType | null>(null);
--- src/context/provideMapContext.tsx
+++ src/context/provideMapContext.tsx
@@ -1,6 +1,8 @@
-import { useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { MapContext } from "./createMapContext";
 import type { Report } from "../pages/map/types";
+import type { FeatureCollection, MultiPolygon, Polygon } from "geojson";
+import { loadBoundary } from "../pages/map/utils/utils";
 
 export function MapContextProvider({
   children,
@@ -10,13 +12,33 @@ export function MapContextProvider({
   const [selectedReport, setSelectedReport] = useState<Report | null>(null);
   const markerRefs = useRef<Record<string, L.Marker>>({});
   const lastFlownReport = useRef<string>(null);
+  const nairobiSubCountyShapefile =
+    useRef<FeatureCollection<Polygon | MultiPolygon>>(null);
+  const [isNotInNairobi, setIsNotInNairobi] = useState(false);
+
+  useEffect(() => {
+    async function loadShapefile() {
+      try {
+        if (!nairobiSubCountyShapefile.current) {
+          const data = await loadBoundary();
+          nairobiSubCountyShapefile.current = data;
+        }
+      } catch (err) {
+        console.error(err);
+      }
+    }
+    loadShapefile();
+  }, []);
   return (
     <MapContext.Provider
       value={{
+        isNotInNairobi,
+        setIsNotInNairobi,
         selectedReport,
         setSelectedReport,
         markerRefs,
         lastFlownReport,
+        nairobiSubCountyShapefile,
       }}
     >
       {children}
