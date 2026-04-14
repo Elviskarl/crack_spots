4fb5d087284cf2e7e4c7197b25b0564563db3645
elvis <elviskarl343@gmail.com>
2026-04-14 19:22:26 +0300

    Refactored CreateIssues function to simplify code structure.

--- src/pages/map/utils/CreateIssues.tsx
+++ src/pages/map/utils/CreateIssues.tsx
@@ -1,14 +1,10 @@
-import { useMemo } from "react";
 import type { Report } from "../types";
 
 export default function CreateIssues(param: Report[]) {
-  const issues = useMemo(() => {
-    const grouped = Object.groupBy(param, (report) => report.issueId);
+  const grouped = Object.groupBy(param, (report) => report.issueId);
 
-    return Object.entries(grouped).map(([issueId, reports]) => ({
-      issueId,
-      reports: reports ?? [],
-    }));
-  }, [param]);
-  return issues;
+  return Object.entries(grouped).map(([issueId, reports]) => ({
+    issueId,
+    reports: reports ?? [],
+  }));
 }
