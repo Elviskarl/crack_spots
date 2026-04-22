1da55767af433bf3539e61dc6e1b94cb2afc015d
elvis <elviskarl343@gmail.com>
2026-04-22 15:35:54 +0300

    Replaced the search icon and enhance the search input layout.

--- src/pages/map/components/sidebar/components/SearchListSection.tsx
+++ src/pages/map/components/sidebar/components/SearchListSection.tsx
@@ -1,6 +1,6 @@
 import { useContext, useEffect, useMemo, useState } from "react";
 import { ReportContext } from "../../../../../context/createReportContext";
-import searchIconUrl from "../../../../../assets/searchIcon.png";
+import searchIconUrl from "../../../../../assets/search-icon.svg";
 import SearchSuggestions from "./SearchSuggestions";
 import MatchingReports from "./MatchingReports";
 import "../../../styles/searchListSection.css";
@@ -75,7 +75,30 @@ export default function SearchListSection(props: ListItemOptional) {
           }}
           onBlur={() => setIsOpen(false)}
           onFocus={() => searchTerm && setIsOpen(true)}
+          maxLength={20}
         />
+        {searchTerm && (
+          <button
+            className="clear-btn"
+            onClick={() => {
+              setSearchTerm("");
+              setTimeout(() => {
+                setIsOpen(false);
+              }, 300);
+            }}
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="ionicon"
+              viewBox="0 0 512 512"
+            >
+              <path
+                d="M289.94 256l95-95A24 24 0 00351 127l-95 95-95-95a24 24 0 00-34 34l95 95-95 95a24 24 0 1034 34l95-95 95 95a24 24 0 0034-34z"
+                fill="currentColor"
+              />
+            </svg>
+          </button>
+        )}
         <button className="search-button">
           <img
             src={searchIconUrl}
